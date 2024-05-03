70cb6272b55774509af09a7bb76e169de4472a66
JoArtMahilaga <Joartmahilaga69@gmail.com>
2024-05-03 23:04:35 +0100

    fixing weapon opacity

--- app/weapons/page.tsx
+++ app/weapons/page.tsx
@@ -18,7 +18,8 @@ console.log(weapons);
     <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6 mt-4">
         {/* Weapon Cards Loop */}
         {(weapons as { weapon_id: number, description: string, attack: string, type: string, rarity: string, image_path: string }[]).map((weapon, index) => (
-            <div key={weapon.weapon_id} className="bg-white text-black rounded-lg shadow-md p-4">
+
+            <div key={weapon.weapon_id} style={{backgroundColor: 'rgba(255, 255, 255, 0.3)'}} className="bg-white text-black rounded-lg shadow-md p-4">
               <img src={`/images/${weapon.image_path}`} alt={weapon.description} className="w-full h-64 object-cover rounded-t-lg" />
                 <h2 className="mt-2 font-bold">{weapon.type}</h2>
                 <p className="text-sm">{weapon.description}</p>
