877c6ab6683a456ed49086d2331569c91e05ca6b
JoArtMahilaga <Joartmahilaga69@gmail.com>
2024-05-03 22:30:05 +0100

    opacity change

--- app/characters/page.tsx
+++ app/characters/page.tsx
@@ -17,8 +17,8 @@ return (
   <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6 mt-4">
       {/* Weapon Cards Loop */}
       {(characters as { character_id: number, name: string, gender: string, element: string, region: string, image_path: string }[]).map((character, index) => (
-          <div key={character.character_id} className="bg-white text-black rounded-lg shadow-md p-4">
-            <img src={`/images/${character.image_path}`} alt={character.name} className="w-full h-64 object-cover rounded-t-lg" />
+        <div key={character.character_id} style={{backgroundColor: 'rgba(255, 255, 255, 0.3)'}} className="text-black rounded-lg shadow-md p-4">         
+           <img src={`/images/${character.image_path}`} alt={character.name} className="w-full h-64 object-cover rounded-t-lg" />
               <h2 className="mt-2 font-bold">{character.element}</h2>
               <p className="text-sm">{character.region}</p>
               <p className="mt-2"><strong>gender:</strong> {character.gender}</p>
